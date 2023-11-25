92c1812783ddbe34bea292ec7888d04ef35a33f4
nassarahmad <ahmad.m.nassarr@gmail.com>
2023-11-25 04:02:59 +0300

    search fillter

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let left_btn=document.getElementsByClassName('bi-chevron-left')[0];
 let right_btn=document.getElementsByClassName('bi-chevron-right')[0];
 let cards=document.getElementsByClassName('cards')[0];
+let search=document.getElementsByClassName('search')[0];
+let search_input=document.getElementsById('search_input');
 left_btn.addEventListener('click',()=>{
     cards.scrollLeft -=140;
 })
@@ -34,6 +36,48 @@ fetch(json_url).then(Response =>Response.json())
     document.getElementById('title').innerText=data[0].name;
     document.getElementById('gen').innerText=data[0].genre;
     document.getElementById('date').innerText=data[0].date;
-    document.getElementById('rate').innerHTML=`<span>imdb</span></p><i class="bi bi-star-fill">9.6</i>${date[0].imdb}`
-    
+    document.getElementById('rate').innerHTML=`<span>imdb</span></p><i class="bi bi-star-fill">9.6</i>${data[0].imdb}`
+    // search data load
+    data.forEach(element =>{
+        let {name,imdb,date,sposter,genre,url,}=element;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML=`
+        <img src="${sposter}" alt="" >
+        <div class="cont">
+         
+         
+             <h3>${name}</h3>
+            
+                 <p>${genre},${date},<span>${imdb}</span></p><i class="bi bi-star-fill">9.6</i>${imdb}</p>
+                 
+             </div>
+        `
+        search.appendChild(card);
+
+
+    })
+    // search fillter
+    search_input. addEventListener('keyup', () => {
+        let filter = search_input. value. toUpperCase();
+        let a = search .getElementsByTagName('a');
+        for (let index = 0; index < a.length;index++) {
+        let b = a[index] .getElementsByClassName ('cont')[e];
+        // console. Log(a.textContent)
+        let TextValue = b. textContent || b.innerText;
+        if(TextValue. toUpperCase().index0f(filter) > -1) {
+       a[index].style.display="flex";
+       search.style.visibility="visible";
+       search.style.opacity=1;
+        } else{
+            a[index].style.display="none"
+        }
+        if(search_input.value==0){
+            search.style.visibility="hidden";
+       search.style.opacity=1;
+        }
+    }
+
+})
 })
