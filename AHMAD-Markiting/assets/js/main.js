1644238ac8be50189ad7c81859eed4a92ff0eb59
nassarahmad <ahmad.m.nassarr@gmail.com>
2023-11-27 05:09:49 +0300

    dark and white mood using toggle

--- AHMAD-Markiting/assets/js/main.js
+++ AHMAD-Markiting/assets/js/main.js
@@ -198,3 +198,21 @@ function CartBoxComponent(title, price, imgSrc) {
 // // update the cart and total on initial page load
 // updateTotal();
 // addEvents();
+
+
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
